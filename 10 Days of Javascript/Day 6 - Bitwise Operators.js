b8ca283f0c2a35e39f3241c764614ad86c1fabe4
Shanks <shanks2999@outlook.com>
2018-07-25 16:12:04 -0500

    added day 6

---
+++ 10 Days of Javascript/Day 6 - Bitwise Operators.js
@@ -0,0 +1,57 @@
+/**
+ Link to Original Problem:
+ https://www.hackerrank.com/challenges/js10-bitwise/problem
+ **/
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getMaxLessThanK(n, k)
+{
+    let max = 0;
+    for(let i=1;i<=n;i++)
+    {
+        for(let j=i+1;j<=n;j++)
+        {
+            // let b1 = i.toString(2);
+            // let b2 = j.toString(2);
+            let num = i & j;
+            if(max < num && num < k)
+                max = num;
+        }
+    }
+    return max;
+}
+
+
+function main() {
+    const q = +(readLine());
+
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+
+        console.log(getMaxLessThanK(n, k));
+    }
+}
