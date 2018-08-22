7813d7257e8616e2beff66162dc728f00c245838
Shanks <shanks2999@outlook.com>
2018-08-22 11:49:40 -0500

    added more

---
+++ Language Proficiency/Java/src/Valid_Username_Regular_Expression.java
@@ -0,0 +1,32 @@
+/**
+ Link to Original Problem:
+ https://www.hackerrank.com/challenges/valid-username-checker/problem
+ **/
+
+
+
+import java.util.Scanner;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "[a-zA-Z]{1}[a-zA-Z0-9_]{7,29}";
+}
+
+
+public class Valid_Username_Regular_Expression {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
